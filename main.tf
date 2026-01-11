3064c0451c13343fb59a1a81e1f062046bdb66ad
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:39:23 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,62 @@
+resource "azurerm_cosmosdb_gremlin_graph" "cosmosdb_gremlin_graphs" {
+  for_each = var.cosmosdb_gremlin_graphs
+
+  account_name           = each.value.account_name
+  database_name          = each.value.database_name
+  name                   = each.value.name
+  partition_key_path     = each.value.partition_key_path
+  resource_group_name    = each.value.resource_group_name
+  analytical_storage_ttl = each.value.analytical_storage_ttl
+  default_ttl            = each.value.default_ttl
+  partition_key_version  = each.value.partition_key_version
+  throughput             = each.value.throughput
+
+  dynamic "autoscale_settings" {
+    for_each = each.value.autoscale_settings != null ? [each.value.autoscale_settings] : []
+    content {
+      max_throughput = autoscale_settings.value.max_throughput
+    }
+  }
+
+  dynamic "conflict_resolution_policy" {
+    for_each = each.value.conflict_resolution_policy != null ? [each.value.conflict_resolution_policy] : []
+    content {
+      conflict_resolution_path      = conflict_resolution_policy.value.conflict_resolution_path
+      conflict_resolution_procedure = conflict_resolution_policy.value.conflict_resolution_procedure
+      mode                          = conflict_resolution_policy.value.mode
+    }
+  }
+
+  dynamic "index_policy" {
+    for_each = each.value.index_policy != null ? [each.value.index_policy] : []
+    content {
+      automatic = index_policy.value.automatic
+      dynamic "composite_index" {
+        for_each = index_policy.value.composite_index != null ? [index_policy.value.composite_index] : []
+        content {
+          index {
+            order = composite_index.value.index.order
+            path  = composite_index.value.index.path
+          }
+        }
+      }
+      excluded_paths = index_policy.value.excluded_paths
+      included_paths = index_policy.value.included_paths
+      indexing_mode  = index_policy.value.indexing_mode
+      dynamic "spatial_index" {
+        for_each = index_policy.value.spatial_index != null ? [index_policy.value.spatial_index] : []
+        content {
+          path = spatial_index.value.path
+        }
+      }
+    }
+  }
+
+  dynamic "unique_key" {
+    for_each = each.value.unique_key != null ? [each.value.unique_key] : []
+    content {
+      paths = unique_key.value.paths
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,74 @@
+variable "cosmosdb_gremlin_graphs" {
+  description = <<EOT
+Map of cosmosdb_gremlin_graphs, attributes below
+Required:
+    - account_name
+    - database_name
+    - name
+    - partition_key_path
+    - resource_group_name
+Optional:
+    - analytical_storage_ttl
+    - default_ttl
+    - partition_key_version
+    - throughput
+    - autoscale_settings (block):
+        - max_throughput (optional)
+    - conflict_resolution_policy (block):
+        - conflict_resolution_path (optional)
+        - conflict_resolution_procedure (optional)
+        - mode (required)
+    - index_policy (block):
+        - automatic (optional)
+        - composite_index (optional, block):
+            - index (required, block):
+                - order (required)
+                - path (required)
+        - excluded_paths (optional)
+        - included_paths (optional)
+        - indexing_mode (required)
+        - spatial_index (optional, block):
+            - path (required)
+    - unique_key (block):
+        - paths (required)
+EOT
+
+  type = map(object({
+    account_name           = string
+    database_name          = string
+    name                   = string
+    partition_key_path     = string
+    resource_group_name    = string
+    analytical_storage_ttl = optional(number)
+    default_ttl            = optional(number)
+    partition_key_version  = optional(number)
+    throughput             = optional(number)
+    autoscale_settings = optional(object({
+      max_throughput = optional(number)
+    }))
+    conflict_resolution_policy = optional(object({
+      conflict_resolution_path      = optional(string)
+      conflict_resolution_procedure = optional(string)
+      mode                          = string
+    }))
+    index_policy = optional(object({
+      automatic = optional(bool, true)
+      composite_index = optional(object({
+        index = object({
+          order = string
+          path  = string
+        })
+      }))
+      excluded_paths = optional(set(string))
+      included_paths = optional(set(string))
+      indexing_mode  = string
+      spatial_index = optional(object({
+        path = string
+      }))
+    }))
+    unique_key = optional(object({
+      paths = set(string)
+    }))
+  }))
+}
+
